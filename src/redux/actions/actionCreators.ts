9de366ae162c265e0eafbc90ecacc23197fea7b2
David Ferrer <davidferrer3d@gmail.com>
2022-02-03 19:14:59 +0100

    Add type at dispatch

--- src/redux/actions/actionCreators.ts
+++ src/redux/actions/actionCreators.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { postActionTypes } from './actionTypes';
+import { AppDispatch } from '../store';
 
 const apiUrl = 'https://jsonplaceholder.typicode.com/posts'
 
 export function loadPosts() {
-    return async (dispatch) => {
+    return async (dispatch: AppDispatch) => {
       const { data } = await axios.get(`${apiUrl}`);
       dispatch({
         type: postActionTypes.LOAD_POST,
