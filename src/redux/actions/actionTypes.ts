f3532d81311cd1dda5d90c145b93e2cce98d02f6
David Ferrer <davidferrer3d@gmail.com>
2022-02-03 16:40:02 +0100

    first commit

---
+++ src/redux/actions/actionTypes.ts
@@ -0,0 +1,11 @@
+export interface actionTypesInterface {
+    LOAD_POST: string,
+    DELETE_POST: string,
+}
+
+const postActionTypes: actionTypesInterface = {
+    LOAD_POST: 'LOAD_POST',
+    DELETE_POST: 'DELETE_POST',
+}
+
+export default postActionTypes;
