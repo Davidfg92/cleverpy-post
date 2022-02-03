854238402484939deb6d1acf8a1db7154db81737
David Ferrer <davidferrer3d@gmail.com>
2022-02-03 18:55:20 +0100

    Create actionTypes

--- src/redux/actions/actionTypes.ts
+++ src/redux/actions/actionTypes.ts
@@ -1,11 +1,6 @@
-export interface actionTypesInterface {
-    LOAD_POST: string,
-    DELETE_POST: string,
-}
+import { actionTypesInterface } from "../../interfaces/actionTypesInterface";
 
-const postActionTypes: actionTypesInterface = {
+export const postActionTypes: actionTypesInterface = {
     LOAD_POST: 'LOAD_POST',
     DELETE_POST: 'DELETE_POST',
 }
-
-export default postActionTypes;
