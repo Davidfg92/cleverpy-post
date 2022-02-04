24466925bb983a1ede8a5a7847f507ca596533f7
David Ferrer <davidferrer3d@gmail.com>
2022-02-04 17:00:52 +0100

    create footer and footers styles

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+ export const Footer = function () {
+  return (
+    <footer className="footer">
+      <span className="footer__span">All rights reserved - David Ferrer 2022</span>
+    </footer>
+  );
+};
