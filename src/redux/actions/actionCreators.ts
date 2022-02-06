61094f94e2d8093c3411c7aecd1225d1e10479d9
David Ferrer <davidferrer3d@gmail.com>
2022-02-06 21:00:51 +0100

    Add deletePost actionCreator

--- src/redux/actions/actionCreators.ts
+++ src/redux/actions/actionCreators.ts
@@ -5,11 +5,20 @@ import { AppDispatch } from '../store';
 const apiUrl = 'https://jsonplaceholder.typicode.com/posts'
 
 export function loadPosts() {
-    return async (dispatch: AppDispatch) => {
-      const { data } = await axios.get(`${apiUrl}`);
-      dispatch({
-        type: postActionTypes.LOAD_POST,
-        payload: data,
-      });
-    };
-  }
+  return async (dispatch: AppDispatch) => {
+    const { data } = await axios.get(`${apiUrl}`);
+    dispatch({
+      type: postActionTypes.LOAD_POST,
+      payload: data,
+    });
+  };
+}
+
+export function deletePost(id:number) {
+   return async (dispatch: AppDispatch) => {
+     dispatch({
+       type: postActionTypes.DELETE_POST,
+       payload: id,
+     })
+   }
+}
