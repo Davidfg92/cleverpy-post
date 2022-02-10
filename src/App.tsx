111b1bc45630314faffb3883e04edc0ec3bd8563
David Ferrer <davidferrer3d@gmail.com>
2022-02-10 20:06:14 +0100

    format component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Footer } from './components/Footer/Footer';
-import { List } from './components/List/List';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Footer } from "./components/Footer/Footer";
+import { List } from "./components/List/List";
 
 function App() {
-
   return (
     <div className="App">
-      <List/>
-      <Footer/>
+      <List />
+      <Footer />
     </div>
   );
 }
