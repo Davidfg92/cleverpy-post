cf40720203ed1387bf8c5367b49603c4b225cb60
David Ferrer <davidferrer3d@gmail.com>
2022-02-12 22:51:18 +0100

    Change logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 import { List } from "./components/List/List";
+import { useDispatch, useSelector } from "react-redux";
+import { Post } from "./interfaces/postInterface";
+import { loadPosts } from "./redux/actions/actionCreators";
 
 function App() {
+  const posts = useSelector((state: Post[]) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadPosts());
+  }, []);
+
   return (
     <div className="App">
-      <List />
+      {posts.length > 0 && <List posts={posts} />}
       <Footer />
     </div>
   );
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,30 +1,42 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Post } from "../../interfaces/postInterface";
-import {
-  loadPosts,
-  deletePost,
-  editPost,
-} from "../../redux/actions/actionCreators";
+import { loadPosts } from "../../redux/actions/actionCreators";
 import { Card } from "../Card/Card";
 import "./List.scss";
 
-export function List() {
-  const dispatch = useDispatch();
-  const posts = useSelector((state: Array<Post>) => state);
+export type ListProps = {
+  posts: Post[];
+};
+
+export const List = ({ posts }: ListProps) => {
+  const [search, setSearch] = useState("");
+  const [list, setList] = useState<Post[]>(posts);
 
   useEffect(() => {
-    dispatch(loadPosts());
-  }, []);  
+    if (search === "") return setList(posts);
+    const newList = posts.filter((el) =>
+      el.title.toLowerCase().includes(search.trim().toLowerCase())
+    );
+    return setList(newList);
+  }, [search, posts]);
 
   return (
     <>
-      <h1 className="title">Posts</h1>
+      <h1 className="title">prueba tecnica cleverpy</h1>
+      <div className="search">
+        <input
+          className="search__input"
+          value={search}
+          type="text"
+          placeholder="Search by title..."
+          onChange={(ev) => setSearch(ev.target.value)}
+        />
+      </div>
       <div className="list">
-        {posts.map((post) => (
-          <Card key={post.id} post={post}/>
+        {list.map((post) => (
+          <Card key={post.id} post={post} />
         ))}
       </div>
     </>
   );
-}
+};
